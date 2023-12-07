1f51b1bd6922820b6a8588ec4f13141d6a9e2cc6
claytonm17 <133607487+claytonm17@users.noreply.github.com>
2023-12-07 18:37:22 -0500

    add movement to box in strict directions

--- snake.py
+++ snake.py
@@ -5,6 +5,10 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+dt = 0
+
+
+player_position = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
 
@@ -13,9 +17,25 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    screen.fill(("black"))
+
     # Render game here
+    pygame.draw.rect(screen, 'green', (player_position.x, player_position.y, 50, 50))
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_position.y -= 300 * dt
+    elif keys[pygame.K_s]:
+        player_position.y += 300 * dt
+    elif keys[pygame.K_a]:
+        player_position.x -= 300 * dt
+    elif keys[pygame.K_d]:
+        player_position.x += 300 * dt
+
+    # Update display
+    pygame.display.flip()
 
     # limiting FPS to 60
-    clock.tick(60)
+    dt = clock.tick(60) / 1000
 
 pygame.quit()
