a2c35255e6d5fc6fb75fdec688c9406136538e1c
claytonm17 <133607487+claytonm17@users.noreply.github.com>
2024-01-15 16:02:12 -0500

    Cannot reverse direction of snake now

--- snake.py
+++ snake.py
@@ -22,11 +22,12 @@ rect_size = 20
 
 food_position = [random.randint(0, width - 1) * grid_size, random.randint(0, height - 1) * grid_size]
 
-current_direction = (0, 0)
+current_direction = (1, 0)
+
+previous_key = None
 
 while running:
 
-    # Allow closing the app
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -37,7 +38,6 @@ while running:
         player_body[i][0] = player_body[i - 1][0]
         player_body[i][1] = player_body[i - 1][1]
 
-    # Render game here
     pygame.draw.rect(screen, (255,0,0), (player_position[0], player_position[1], rect_size, rect_size))
 
     for segment in player_body:
@@ -45,14 +45,18 @@ while running:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_w]:
+    if keys[pygame.K_w] and previous_key != "s":
         current_direction = (0, -1)
-    elif keys[pygame.K_s]:
+        previous_key = "w"
+    elif keys[pygame.K_s] and previous_key != "w":
         current_direction = (0, 1)
-    elif keys[pygame.K_a]:
+        previous_key = "s"
+    elif keys[pygame.K_a] and previous_key != "d":
         current_direction = (-1, 0)
-    elif keys[pygame.K_d]:
+        previous_key = "a"
+    elif keys[pygame.K_d] and previous_key != "a":
         current_direction = (1, 0)
+        previous_key = "d"
 
     player_position[0] += current_direction[0] * grid_size
     player_position[1] += current_direction[1] * grid_size
@@ -74,16 +78,16 @@ while running:
     player_body[0][0] = player_position[0]
     player_body[0][1] = player_position[1]
 
-    # Check for collision with body
-    
+    for segment in player_body[1:]:
+        if player_position[0] == segment[0] and player_position[1] == segment[1]:
+            print("YOU LOSE")
+            running = False
 
-    # Rendering grid lines
     for x in range(0, 720, grid_size):
         pygame.draw.line(screen, (100, 100, 100), (x, 0), (x, 480))
     for y in range(0, 480, grid_size):
         pygame.draw.line(screen, (100, 100, 100), (0, y), (720, y))
 
-    # Food
     pygame.draw.rect(screen, (0, 0, 255), (food_position[0], food_position[1], grid_size, grid_size))
 
     if (
